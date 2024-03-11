2bdcc22d3621679473cf95b05e6305003d834abb
MisterG <g.haegerbaeumer@gmx.de>
2024-03-11 18:35:01 +0100

    Changd Ticker to Work

--- Assets/Scripts/Data/TickerQueue.cs
+++ Assets/Scripts/Data/TickerQueue.cs
@@ -1,130 +1,49 @@
-using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Data
 {
     public class TickerQueue<TItem>
     {
-        private TickerQueueNode _first;
-        private TickerQueueNode _last;
-        private TickerQueueNode _current;
-        
-        public TickerQueue(TItem pItem, int pPriority)
-        {
-            _first = _last = new TickerQueueNode(pItem, pPriority);
-            Debug();
-        }
-
-        public void Debug()
-        {
-            Console.Out.WriteLine($"f: {_first}|l: {_last}");
-        }
+        private Dictionary<TItem, int> _intern = new();
         
         public void Enqueue(TItem pItem, int pPriority)
         {
-            if (IsEmpty())
-            {
-                _first = _last = new TickerQueueNode(pItem, pPriority);
-            }
-            else if (_first.Priority > pPriority)
-            {
-                var newNode = new TickerQueueNode(pItem, pPriority)
-                {
-                    NextNode = _first
-                };
-                _first = newNode;
-            }
-            else
-            {
-                TickerQueueNode newNode = new TickerQueueNode(pItem, pPriority);
-                ToFirst();
-                while (HasAccess())
-                {
-                    if (_current.Priority > pPriority)
-                    {
-                        newNode.NextNode = _current;
-                        PreviousNode(_current).NextNode = newNode;
-                        Debug();
-                        return;
-                    }
-                    Next();
-                }
-
-                _last.NextNode = newNode;
-                _last = newNode;
-            }
-
-            Debug();
+            _intern[pItem] = pPriority;
         }
 
+        public void ManualCleanUp()
+        {
+            var max = _intern.Aggregate((l, r) => l.Value < r.Value ? l : r).Value;
+            if (max != 0)
+            {
+                LowerAllBy(max);
+            }
+        }
+        
         public TItem Dequeue()
         {
-            if (IsEmpty()) return default;
-            TickerQueueNode tmp = _first;
-            if (_first == _last)
-            {
-                _first = _last = null;
-                return tmp.Item;
-            }
-
-            _first = _first.NextNode;
-            LowerAllBy(tmp.Priority);
-            return tmp.Item;
+            var max = _intern.Aggregate((l, r) => l.Value < r.Value ? l : r).Key;
+            var val = _intern[max];
+            _intern.Remove(max);
+            LowerAllBy(val);
+            return max;
         }
 
-        private void LowerAllBy(int amount)
+        public KeyValuePair<TItem, int>[] GetCurrentState()
         {
-            ToFirst();
-            while (HasAccess())
-            {
-                _current.Priority -= amount;
-                Next();
-            }
-        }
-        
-        private TickerQueueNode PreviousNode(TickerQueueNode pNode)
-        {
-            var tmp = _first;
-            while (tmp is not null)
-            {
-                if (tmp.NextNode == pNode) return tmp;
-                tmp = tmp.NextNode;
-            }
-
-            return null;
-        }
-        
-        public void ToFirst()
-        {
-            _current = _first;
-        }
-        
-        public bool HasAccess()
-        {
-            return _current is null;
-        }
-
-        public void Next()
-        {
-            _current = _current.NextNode;
+            return (from entry in _intern orderby entry.Value ascending select entry).ToArray();
         }
         
         public bool IsEmpty()
         {
-            return _first is null;
+            return _intern.Count == 0;
         }
         
-        private class TickerQueueNode
+        private void LowerAllBy(int amount)
         {
-            public readonly TItem Item;
-            public int Priority;
-
-            public TickerQueueNode NextNode;
-
-            public TickerQueueNode(TItem pItem, int pPriority)
-            {
-                Item = pItem;
-                Priority = pPriority;
-            }
+            if (amount == 0) return;
+            _intern = _intern.ToDictionary(p => p.Key, p => p.Value - amount);
         }
     }
 }
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,29 +1,9 @@
-using Data;
 using UnityEngine;
 
 namespace Managers
 {
     public class GameManager : MonoBehaviour
     {
-        private TickerQueue<string> _test;
-
-        private void Awake()
-        {
-            print("Test Begins");
-            
-            _test = new TickerQueue<string>("Hallo", 10);
-            _test.Enqueue("Welt", 11);
-            
-            print(_test.IsEmpty());
-            
-            _test.ToFirst();
-            print(_test.HasAccess());
-            
-            while (_test.HasAccess())
-            {
-                print(_test.Dequeue() is null);
-                _test.Next();
-            }
-        }
+        
     }
 }
