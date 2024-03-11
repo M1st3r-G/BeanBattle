39ff74478f96cd9e2b4ea1d894acc4a655c3f6fa
MisterG <g.haegerbaeumer@gmx.de>
2024-03-11 18:50:22 +0100

    Added Simple turn detection

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,9 +1,32 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Managers
 {
     public class GameManager : MonoBehaviour
     {
+        [SerializeField] private InputActionReference nextPhaseAction;
+        private bool _nextPhasePressed;
         
+        private IEnumerator Start()
+        {
+            print("SetUp");
+            yield return new WaitForSeconds(0.5f);
+            
+            while(true){
+                print("PlayerPhase");
+                _nextPhasePressed = false;
+                nextPhaseAction.action.Enable();
+
+                yield return new WaitUntil(() => _nextPhasePressed);
+                nextPhaseAction.action.Disable();
+            }
+        }
+
+        private void OnEnable()
+        {
+            nextPhaseAction.action.performed += ctx => _nextPhasePressed = true;
+        }
     }
 }
