34080402012043d93b2b8dad33b3c806a1f9aa4e
M1st3r-G <g.haegerbaeumer@gmx.de>
2024-05-08 12:52:44 +0200

    First AttackLogic

--- Assets/Scripts/Data/CharacterStates/CharacterAttackState.cs
+++ Assets/Scripts/Data/CharacterStates/CharacterAttackState.cs
@@ -1,3 +1,4 @@
+using Managers;
 using UnityEngine;
 
 namespace Data.CharacterStates
@@ -5,8 +6,18 @@ namespace Data.CharacterStates
     [CreateAssetMenu(fileName = "Attack", menuName = "States/Attack", order = 10)]
     public class CharacterAttackState : CharacterStateBase
     {
+        private int attackRange;
         public override bool ExecuteStateFrame() => false;
-        public override void InternalStateSetUp() { }
-        public override void StateDisassembly(){ }
+
+        public override void InternalStateSetUp()
+        {
+            attackRange = ActiveCharacter.GetData.AttackRange;
+            GridManager.Instance.DisplayRange(ActiveCharacter, attackRange);
+        }
+
+        public override void StateDisassembly()
+        {
+            GridManager.Instance.ResetRange();
+        }
     }
 }
