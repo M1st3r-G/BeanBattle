98a59f76eef40f56b0d6fe36300a31685da0d068
MisterG <g.haegerbaeumer@gmx.de>
2024-05-13 12:40:37 +0200

    Added AttackState Stuff

--- Assets/Scripts/Data/CharacterStates/CharacterAttackState.cs
+++ Assets/Scripts/Data/CharacterStates/CharacterAttackState.cs
@@ -1,3 +1,4 @@
+using Controller;
 using Managers;
 using UnityEngine;
 
@@ -7,17 +8,66 @@ namespace Data.CharacterStates
     public class CharacterAttackState : CharacterStateBase
     {
         private int attackRange;
-        public override bool ExecuteStateFrame() => false;
+        private CharController currentSelection;
+        
+        public override bool ExecuteStateFrame()
+        {
+            if (currentSelection is null)
+            {
+                return false;
+            }
+            else
+            {
+                Debug.Log("Wait For Accept"); 
+            }
+            return false;
+        }
 
         public override void InternalStateSetUp()
         {
+            currentSelection = null;
             attackRange = ActiveCharacter.GetData.AttackRange;
             GridManager.Instance.DisplayRange(ActiveCharacter, attackRange);
+
+            int numOfChars = GridManager.Instance.CharactersInRange(out CharController[] charsInRange);
+            if (numOfChars == 1)
+            {
+                Debug.LogError("There is no one in Range");
+            }
+            if(numOfChars == 2){
+                Debug.LogWarning("Found only two Characters");
+                //SetSelection(charsInRange);
+            }
+            else
+            {
+                MouseInputManager.OnCharacterClicked += Stuff;
+            }
+        }
+
+        private void SetSelection(CharController c)
+        {
+            if (currentSelection is not null) c.SetSelector(false);
+
+            currentSelection = c;
+            c.SetSelector(true);
+        }
+        
+        private void Stuff(CharController hitCharacter)
+        {
+            if (hitCharacter != ActiveCharacter)
+            {
+                Debug.LogWarning($"Target {hitCharacter.gameObject.name}");
+                SetSelection(hitCharacter);
+            }
+
+            Debug.LogWarning("Target Self");
         }
 
         public override void StateDisassembly()
         {
+            if (currentSelection is not null) currentSelection.SetSelector(false);
             GridManager.Instance.ResetRange();
+            MouseInputManager.OnCharacterClicked -= Stuff;
         }
     }
 }
