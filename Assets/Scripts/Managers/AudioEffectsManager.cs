64aa4be4f8b9de0ef383d5437f666bd4a070f743
M1st3r-G <g.haegerbaeumer@gmx.de>
2024-05-30 20:09:10 +0200

    Added More sound Files

--- Assets/Scripts/Managers/AudioEffectsManager.cs
+++ Assets/Scripts/Managers/AudioEffectsManager.cs
@@ -1,16 +1,18 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Managers
 {
     [RequireComponent(typeof(AudioSource))]
     public class AudioEffectsManager : MonoBehaviour
     {
-        [SerializeField] private AudioClip[] clips;
+        [SerializeField] private AudioClipsContainer[] clips;
         private AudioSource src;
 
         public enum AudioEffect
         {
-            Debug
+            Click, Attack, Move, Hit, Death, Evil, Heal
         }
         
         private void Awake()
@@ -20,7 +22,31 @@ namespace Managers
 
         public void PlayEffect(AudioEffect effect)
         {
-            src.PlayOneShot(clips[(int)effect]);
+            foreach (AudioClipsContainer cnt in clips)
+            {
+                if (cnt.Type == effect)
+                {
+                    src.PlayOneShot(cnt.GetClip());
+                    return;
+                }
+            }
+            
+            Debug.LogError($"Sound with {effect} Identifier not found");
+        }
+
+        [Serializable]
+        private struct AudioClipsContainer
+        {
+            public string name;
+            [SerializeField] private AudioEffect type;
+            [SerializeField] private AudioClip[] clips;
+
+            public AudioEffect Type => type;
+            
+            public AudioClip GetClip()
+            {
+                return clips[Random.Range(0, clips.Length)];
+            }
         }
     }
 }
